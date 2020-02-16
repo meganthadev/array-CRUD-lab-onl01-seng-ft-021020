bc1e2b7bd50420133a0b725595385327367dd422
Megan Horsfield <meganthadev@gmail.com>
2020-02-16 00:31:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
   
 def create_an_array
-  tea = ["chamomille", "chai", "black", "green"]
+  tea = ["chamomille","chai","black","green"]
 end
 
 def add_element_to_end_of_array(array, element)
