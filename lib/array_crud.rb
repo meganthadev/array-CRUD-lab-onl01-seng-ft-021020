2e8be38afd288c6058f04f035f7eafa0e4b0d330
Megan Horsfield <meganthadev@gmail.com>
2020-02-16 00:28:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,11 +23,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  tea[1]
+  element[1]
 end
 
 def retrieve_first_element_from_array(array)
-  tea[0]
+  element[0]
 end
 
 def retrieve_last_element_from_array(array)
