e6359da6569a5e0ea63050135b40f2538a8c401a
Megan Horsfield <meganthadev@gmail.com>
2020-02-16 00:22:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+   []
 end
-
-def create_an_array
   
+def create_an_array
+  tea = ["chamomille", "chai", "black", "green"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  tea << "white"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  tea.unshift("Orange Pecoe")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  tea.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  tea.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  tea[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  tea[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  tea[-1]
 end
